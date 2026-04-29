b625f34e2ef7cc6cf78dedbbefda87825698cd5c
Gabbu69 <gabbu69@github.com>
2026-04-29 14:02:22 +0800

    chore: bump service worker cache to v3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agripresyo-v2';
+const CACHE_NAME = 'agripresyo-v3';
 const STATIC_ASSETS = [
   '/',
   '/AgriPresyo_logoFinal.webp',
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(decodeURIComponent("%E2%82%B1"));
